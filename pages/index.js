15cea38abac1112d8db3a4161078a0dd125a3b2a
Carolina Freire <68876784+carolinafreire@users.noreply.github.com>
2023-06-20 22:13:44 +0000

    update

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,7 @@
-<<<<<<< HEAD
 function Home() {
   return (
     <h1>Home</h1>
     
   )
 }
-
-=======
-function Home() {
-  return (
-    <h1>Home</h1>
-    
-  )
-}
-
->>>>>>> 95d1fa6 (update)
 export default Home
